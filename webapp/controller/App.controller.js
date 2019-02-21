868c5f586fc492b286fbf89e412ed5a15d77c0c7
PARIMALAA SAMYAPPAN <parictech@gmail.com>
2019-02-21 04:41:41 +0000

    Added extra line

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -12,6 +12,7 @@ sap.ui.define([
 			this.oRouter.attachRouteMatched(this.onRouteMatched, this);
 			this.oRouter.attachBeforeRouteMatched(this.onBeforeRouteMatched, this);
 		},
+		
 
 		onBeforeRouteMatched: function(oEvent) {
 			var oModel = this.oOwnerComponent.getModel(),
